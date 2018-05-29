f2618143b0bc3e0ab9a210a3f864c28c93ed3a8a
Vladimir Ilin <mark09072010@gmail.com>
2018-05-29 16:09:53 +0300

    stage4 (add brain-even)

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@ import readlineSync from 'readline-sync';
 const greetingUser = () => {
     const userName = readlineSync.question("May I have your name? ");
     console.log(`Hello, ${userName}!`);
+    return userName;
 }
-export default greetingUser;
+const question = (arr, index) => {
+    const answer = readlineSync.question(`Question: ${arr[index]}\n`);
+    console.log(`Your answer: ${answer}`);
+    return answer;
+}
+const isEven = num => num % 2 === 0 ? true : false;
+const randomIndex = arr => Math.floor(Math.random() * arr.length);
+
+export { greetingUser, isEven, randomIndex, question };
