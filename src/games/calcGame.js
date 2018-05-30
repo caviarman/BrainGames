efc4b72e5e4e8c4f5bc8d4ed42cd15096f6f73c2
Vladimir Ilin <mark09072010@gmail.com>
2018-05-31 00:29:04 +0300

    stage 5 (edit calcGame.js)

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -6,19 +6,17 @@ const startCalculatorGame = () => {
   console.log('What is the result of the expression?');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  let userAnswer;
-  let rightAnswer;
   const sum = (num1, num2) => num1 + num2;
   const minus = (num1, num2) => num1 - num2;
   const multi = (num1, num2) => num1 * num2;
-  const operators = ['+', '-', '*'];
-  const actions = [sum, minus, multi];
+  const operator = ['+', '-', '*'];
+  const action = [sum, minus, multi];
   for (let i = 0; i < attempt; i += 1) {
     const num1 = getRandomNum(1, 10);
     const num2 = getRandomNum(1, 10);
     const index = getRandomNum(0, 2);
-    userAnswer = readlineSync.question(`Question: ${num1} ${operators[index]} ${num2}\n`);
-    rightAnswer = actions[index](num1, num2);
+    const userAnswer = readlineSync.question(`Question: ${num1} ${operator[index]} ${num2}\n`);
+    const rightAnswer = action[index](num1, num2);
     console.log(`Your answer: ${userAnswer}`);
     if (Number(userAnswer) === rightAnswer) {
       console.log('Correct!');
