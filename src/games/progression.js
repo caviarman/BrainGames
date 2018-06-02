5dfc10d91cf862e7d7f8dc3d73e35cde541d1fd5
Vladimir Ilin <mark09072010@gmail.com>
2018-06-02 17:52:02 +0300

    stage 8(fixed)

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,25 +5,27 @@ const task = 'What number is missing in this progression?';
 const size = 10;
 const sum = (num1, num2) => num1 + num2;
 const minus = (num1, num2) => num1 - num2;
-const multi = (num1, num2) => num1 * num2;
-const actions = [sum, minus, multi];
+const actions = [sum, minus];
 const getQuestion = (arr, index) => {
-  const newArr = arr;
+  const newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    newArr[i] = arr[i];
+  }
   newArr[index] = '..';
-  return arr;
+  return newArr.join(' ');
 };
-const indexForAction = getRandomNum(0, 2);
-const indexForArr = getRandomNum(0, 9);
-const arr = [];
-
 const gameBalance = () => {
-  const modifier1 = getRandomNum(2, 50);
-  const modifier2 = getRandomNum(10, 99);
-  for (let i = 1; i < size + 1; i += 1) {
-    arr[i - 1] = actions[indexForAction]((modifier1 * i), modifier2);
+  const indexForAction = getRandomNum(0, 1);
+  const indexForArr = getRandomNum(0, 9);
+  const arr = [];
+  let startPoint = getRandomNum(-10, 99);
+  const stepOfProgression = getRandomNum(-99, 99);
+  for (let i = 0; i < size; i += 1) {
+    arr[i] = actions[indexForAction](startPoint, stepOfProgression);
+    startPoint += stepOfProgression;
   }
   const rightAnswer = String(arr[indexForArr]);
-  const question = (getQuestion(arr, indexForArr)).join(' ');
+  const question = getQuestion(arr, indexForArr);
   return { question, rightAnswer };
 };
 export default () => gameEngine(gameBalance, task);
